a9fe8d3def8468729c3d24346438eb85bbdc5329
Axel verglas <axelverglas@outlook.com>
2024-07-11 21:55:57 +0200

    feat: Ajouter la méthode find_or_create_game à QueueService
    La méthode `find_or_create_game` a été ajoutée à la classe `QueueService`. Cette méthode permet aux utilisateurs de trouver un adversaire disponible pour un type de jeu spécifique et de créer un nouveau jeu s'il y a un adversaire disponible. Si aucun adversaire n'est disponible, l'utilisateur est ajouté à la file d'attente.

--- tests/service/test_queue_service.py
+++ tests/service/test_queue_service.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import MagicMock
+from bson import ObjectId
 from src.service.queue_service import QueueService
 
 @pytest.fixture
@@ -19,28 +20,37 @@ def test_find_or_create_game_create_new_game(queue_service, mock_queue_repositor
     game_type = "tictactoe"
     
     # Configure mock to return None, indicating no opponent found
-    mock_queue_repository.get_next_player.return_value = None
+    mock_queue_repository.find_one.return_value = None
 
-    game_id = None
-    opponent_id = None
-    
     result = queue_service.find_or_create_game(user_id, game_type)
 
-    assert result == (game_id, opponent_id)
-    mock_queue_repository.add_to_queue.assert_called_once_with(user_id, game_type)
+    assert result == (None, None)
+    mock_queue_repository.insert_one.assert_called_once_with({
+        'user_id': user_id,
+        'game_type': game_type,
+        'state': 'waiting'
+    })
 
 def test_find_or_create_game_existing_opponent(queue_service, mock_queue_repository, mock_game_service):
     user_id = "user1"
     opponent_id = "user2"
     game_type = "tictactoe"
-    game_id = "game1"
-    mock_game = MagicMock()  # Utilisez un mock d'objet de jeu complet
+    game_id = str(ObjectId())
+    opponent = {
+        '_id': str(ObjectId()),
+        'user_id': opponent_id,
+        'game_type': game_type,
+        'state': 'waiting'
+    }
+    mock_game = MagicMock()
+    mock_game._id = game_id
     
     # Configure mock to return an existing opponent
-    mock_queue_repository.get_next_player.return_value = opponent_id
-    mock_game_service.create_game.return_value = mock_game  # Retourne l'objet de jeu
+    mock_queue_repository.find_one.return_value = opponent
+    mock_game_service.create_game.return_value = mock_game
 
     result = queue_service.find_or_create_game(user_id, game_type)
 
-    assert result == (mock_game._id, opponent_id)  # Utilisez l'ID de l'objet de jeu
-    mock_game_service.create_game.assert_called_once_with(opponent_id, user_id, game_type)
+    assert result == (mock_game._id, opponent_id)
+    mock_game_service.create_game.assert_called_once_with(user_id, opponent_id, game_type)
+    mock_queue_repository.delete_one.assert_called_once_with({'_id': opponent['_id']})
